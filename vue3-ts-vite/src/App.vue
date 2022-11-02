c9e219a9252e5ab60a7db6a722f27c4419872c21
邓宇 <313187515@qq.com>
2022-11-02 23:49:44 +0800

    数组方法

--- vue3-ts-vite/src/App.vue
+++ vue3-ts-vite/src/App.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+const arr = ref<Array<string>>(["a", "b", "c"]);
+const a = ref<string>("a");
+setTimeout(() => {
+  a.value = "123";
+  arr.value.splice(1, 0, "test");
+}, 2000);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  {{ a }}
+  <div v-for="item in arr" :key="item">{{ item }}</div>
 </template>
 
 <style scoped>
